06cf637f8607b58cd50dfcf6d889c8e2db9fb774
alexcheng852 <60951126+alexcheng852@users.noreply.github.com>
2022-04-17 03:00:22 +0800

    add product card and css

--- src/pages/product.js
+++ src/pages/product.js
@@ -2,50 +2,45 @@ import React from "react";
 import productsData from "./productsData";
 import { Link } from "react-router-dom";
 import "./pages.css"
-import "./Category.css"
+
 import {Card, Col, Row} from "react-bootstrap";
 
-const product = () => {
-    const products = productsData.map(product => {
-        return (
-            <div key={product.id}>
-                <div className="col-md-6 col-lg-4 col-xl-2">
-                    <Card style={{ width: '18rem' }}>
-                    <div className="part-1">
-
-                            <a href={`/products/${product.id}`}>
-                                <img className="photo1" src={product.img} />
-                            </a>
-                    </div>
-                    <div className="part-2">
-                            <h3 className="product-brand" href="#">{product.brand}</h3>
-                            <h3 className="product-name" href="#">{product.name}</h3>
-                            <h4 className="product-old-price text-decoration-line-through">${product.price}</h4>
-                            <h4 className="product-new-price">${product.price}</h4>
-                        </div>
-                    </Card>
-
-                    </div>
-                </div>
-
-        );
-    });
-
-
+const product = ({product}) => {
     return (
-        <div className="container">
+        <div className= "products">
             <nav className="nav" aria-label="breadcrumb">
                 <ol className="breadcrumb bg-transparent">
                     <li className="breadcrumb-item"><a href="/">Home</a></li>
-                    <li className="breadcrumb-item active">Products</li>
+                    <li className="breadcrumb-item active">All Products</li>
                 </ol>
             </nav>
-            <h1>Products Page</h1>
-            <div className="row">
+            {productsData.map((product) => (
+            <div className="card">
+                <div>
+                    <a href={`/products/${product.id}`}>
+                        <img className="photo1" src={product.img} />
+                    </a>
+                </div>
+                <div>
+                    <h3 className="product-brand" href="#">{product.brand}</h3>
+                </div>
+                <div>
+                    <h3 className="product-name" href="#">{product.name}</h3>
+                </div>
+                <div>
+                    <h4 className="product-old-price text-decoration-line-through">${product.price}</h4>
+                </div>
+                <div>
+                    <h4 className="product-new-price">${product.price}</h4>
+                </div>
 
-                {products}
             </div>
+
+        ))}
         </div>
-    );
-};
+    )};
+
+
+
 export default product;
+
